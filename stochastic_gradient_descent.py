386fae33374a4fc65f855155166241082250a6ff
FabienDon <fabien.donnet-monay@epfl.ch>
2025-09-22 11:30:20 +0200

    added files from ex2

---
+++ stochastic_gradient_descent.py
@@ -0,0 +1,71 @@
+# -*- coding: utf-8 -*-
+"""Problem Sheet 2.
+
+Stochastic Gradient Descent
+"""
+from helpers import batch_iter
+from costs import compute_loss
+
+
+def compute_stoch_gradient(y, tx, w):
+    """Compute a stochastic gradient at w from just few examples n and their corresponding y_n labels.
+
+    Args:
+        y: shape=(N, )
+        tx: shape=(N,2)
+        w: shape=(2, ). The vector of model parameters.
+
+    Returns:
+        An array of shape (2, ) (same shape as w), containing the stochastic gradient of the loss at w.
+    """
+
+    B = y.shape[0]
+    e = y - tx@w
+
+    grad = -1/B*np.transpose(tx)@e
+    return grad
+
+
+def stochastic_gradient_descent(y, tx, initial_w, batch_size, max_iters, gamma):
+    """The Stochastic Gradient Descent algorithm (SGD).
+
+    Args:
+        y: shape=(N, )
+        tx: shape=(N,2)
+        initial_w: shape=(2, ). The initial guess (or the initialization) for the model parameters
+        batch_size: a scalar denoting the number of data points in a mini-batch used for computing the stochastic gradient
+        max_iters: a scalar denoting the total number of iterations of SGD
+        gamma: a scalar denoting the stepsize
+
+    Returns:
+        losses: a list of length max_iters containing the loss value (scalar) for each iteration of SGD
+        ws: a list of length max_iters containing the model parameters as numpy arrays of shape (2, ), for each iteration of SGD
+    """
+
+    N = y.shape[0]
+
+    # Define parameters to store w and loss
+    ws = [initial_w]
+    losses = []
+    w = initial_w
+
+    for n_iter in range(max_iters):
+
+        batch_indices = np.random.choice(N, batch_size, replace=False)
+        y_batch = y[batch_indices]
+        tx_batch = tx[batch_indices]
+
+        grad = compute_stoch_gradient(y_batch, tx_batch, w)
+        loss = compute_loss(y, tx, w)
+
+        w = w - gamma * grad
+
+        ws.append(w.copy())
+        losses.append(loss)
+
+        print(
+            "SGD iter. {bi}/{ti}: loss={l}, w0={w0}, w1={w1}".format(
+                bi=n_iter, ti=max_iters - 1, l=loss, w0=w[0], w1=w[1]
+            )
+        )
+    return losses, ws
